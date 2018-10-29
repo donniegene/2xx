0fef0f803010e2d914eddf8c6d60f8f0f8e0ac6a
donniegene <da34773@ravens.racc.edu>
2018-10-28 21:35:43 -0400

    added copyright and versioning

--- include/footer.inc.php
+++ include/footer.inc.php
@@ -2,6 +2,7 @@
 				<h4>this is the footer</h4>
 				<p>we place redundant information here</p>
 				<div id="footerTagLine">I am 7 inches left of normal</div>
+				<div class="copyright">iop project - Version <?php echo VERSION; ?> | &copy; 1958 - <?php echo date('Y'); ?> Don Allen</div>
 			</footer>
 
 	</body>
