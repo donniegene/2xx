f2da258eaf56920a2524e8b370dfbc0183bc3c58
donniegene <da34773@ravens.racc.edu>
2018-11-06 14:40:12 -0500

    updated config.inc.php

--- include/config.inc.php
+++ include/config.inc.php
@@ -1,4 +1,5 @@
 <?php
-      define("VERSION", "2.0.3.0");  // provides the current version of the application
+      define("VERSION", "2.0.4.0");  // provides the current version of the application
       define("SITENAME", "inside out | 2xx");  // provides the application name
+      define('DEFUALT_PAGE','home');  // defines the default page of the website
 ?>
